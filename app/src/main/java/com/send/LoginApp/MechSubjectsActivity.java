a623a2b6bf1b9a2ab83e237dbfb4f354d22dce2e
SandeepFrost <sandeep@ace.on;ine>
2023-10-31 12:33:36 +0530

    added calculator

--- app/src/main/java/com/send/LoginApp/MechSubjectsActivity.java
+++ app/src/main/java/com/send/LoginApp/MechSubjectsActivity.java
@@ -7,25 +7,24 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.send.Myapp.R;
 
-public class GateMechanicalActivity extends AppCompatActivity {
+public class MechSubjectsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.coursesactivity);
-        Button gateMech = findViewById(R.id.gatemech);
+        setContentView(R.layout.gatemechactivity);
+        Button buy = findViewById(R.id.BuyCourses);
         Intent intent = getIntent();
         if (intent != null) {
             String message = intent.getStringExtra("Send");
             if (message != null) {
-                Toast.makeText(GateMechanicalActivity.this, "ForgotPassword", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MechSubjectsActivity.this, "ForgotPassword", Toast.LENGTH_SHORT).show();
             }
         }
-
-        gateMech.setOnClickListener(v -> {
-            Intent intent1 = new Intent(GateMechanicalActivity.this, MechSubjectsActivity.class);
-            intent1.putExtra("Gate Mech subjects", "subject are opened");
-            startActivity(intent1);
+        buy.setOnClickListener(v -> {
+                Intent intent1 = new Intent(MechSubjectsActivity.this, BuyCourseActivity.class);
+                intent1.putExtra("You can see all subjects", "Select Course and Buy");
+                startActivity(intent1);
         });
     }
 }
